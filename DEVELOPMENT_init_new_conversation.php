e2c3e815f7c3c3d073fccb1867adca4d41e4ec35
talkfish <enrico@talkfish.de>
2017-05-10 08:07:30 +0200

    added multiple runs and fillrate measurement

--- DEVELOPMENT_init_new_conversation.php
+++ DEVELOPMENT_init_new_conversation.php
@@ -6,40 +6,74 @@ print("<p>Hello, world!</p>");
 
 print("<p>generating new instance</p>");
 
-$endpoint_id_length = 5;
-$endpoint_string_arr = array();
-for($i=0;$i<$endpoint_id_length;$i++) {
-   // mapping of possible randcharcodes
-   //  1 to 26: abcdefghijklmnopqrstuvwxyz
-   // 27 to 36: 0123456789
-   $randcharcode = rand(1,36);
-   $rand_char = '_'; // should not appear in target string
-   if ($randcharcode>=1 && $randcharcode<=26) {
-      $rand_char = chr(ord('a')+$randcharcode-1);
-   }
-   if ($randcharcode>=27 && $randcharcode<=36) {
-      $rand_char = chr(ord('0')+$randcharcode-27);
-   }
-   print($i);
-   print(":");
-   print($randcharcode);
-   print("->");
-   print($rand_char);
-   print("<br/>");
-   $endpoint_string_arr[$i] = $rand_char;
-}
-$endpoint_string = implode($endpoint_string_arr);
 
-print("<p>finally got: "); 
-print($endpoint_string);
-print("</p>"); 
+function generateRandomName($endpoint_id_length) {
+   $endpoint_string_arr = array();
+   for($i=0;$i<$endpoint_id_length;$i++) {
+      // mapping of possible randcharcodes
+      //  1 to 26: abcdefghijklmnopqrstuvwxyz
+      // 27 to 36: 0123456789
+      $randcharcode = rand(1,36);
+      $rand_char = '_'; // should not appear in target string
+      if ($randcharcode>=1 && $randcharcode<=26) {
+         $rand_char = chr(ord('a')+$randcharcode-1);
+      }
+      if ($randcharcode>=27 && $randcharcode<=36) {
+         $rand_char = chr(ord('0')+$randcharcode-27);
+      }
+      // print($i);
+      // print(":");
+      // print($randcharcode);
+      // print("->");
+      // print($rand_char);
+      // print("<br/>");
+      $endpoint_string_arr[$i] = $rand_char;
+   }
+   $endpoint_string = implode($endpoint_string_arr);
+   return $endpoint_string;
+}
+
+$length = 1;
+$prefix = "test/";
+$max_try_counts = 5;
+
+
+
+$success = false;
+$counter = 0;
+do {
+   $counter += 1;
+   $endpoint_string_candidate = generateRandomName($length);
+   $newpath = $prefix . $endpoint_string_candidate;
+   if (!file_exists($newpath)) {
+      mkdir($newpath,0755);
+      $success = true;
+
+      print("<p>"); 
+      print("created directory " . $newpath . " at $counter run(s).");
+      print("</p>"); 
+   } 
+} while ($counter < $max_try_counts and ! $success);
+
+if (!$success) {
+   print("<p>"); 
+   print("ERROR: took $counter runs, but could not find an empty slot.");
+   print("</p>"); 
+}
 
 print("<p>possible number of different combinations: ");
-print(pow(36,$endpoint_id_length));
+$combinations = pow(36,$length);
 print("</p>"); 
-
+print($combinations);
+$dircontent = scandir($prefix);
+if ($dircontent) {
+   $currentamount = count($dircontent) -2;
+   $fillrate = 100 * $currentamount / $combinations;
+   print("<p>currentcount: $currentamount -> fillrate: $fillrate</p>");
+}
+print("<hr>");
 print("<p>TODO: cp template to new instance</p>");
-
-
+print("<p>TODO: prevent multiple parallel executions by an exclusive lock</p>");
+print("<p>TODO: toplevel directories to group subdirs and prevent to many subdirectories</p>");
 
 ?>
